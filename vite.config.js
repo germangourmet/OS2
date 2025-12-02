f728a225f0c5cf552438a056ccefc3959ddd4124
germangourmet <96550199+germangourmet@users.noreply.github.com>
2025-12-01 23:49:54 -0600

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['three'],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ['three'],
+        },
+      },
+    },
+  },
 });
